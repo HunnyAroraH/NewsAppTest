0f39f83d3b434041218b7bafc8971f92bceeae2e
Hunny <parveenarora402@gmail.com>
2020-09-08 00:30:28 +0530

    All done

--- app/src/main/java/com/mvvm/newsapp/MainActivity.kt
+++ app/src/main/java/com/mvvm/newsapp/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("This Is our second commit")println("Change 3")
+        println("This Is our second commit")
         println("Another printline statemen4")
         println("This line will be merged ")
 
